241160fa83ca9df01f8bfdfcaa7c165ca3686e70
Rachel Levin <rachel.levin.md@gmail.com>
2021-09-14 22:46:36 +0300

    new commit

--- index.js
+++ index.js
@@ -35,11 +35,37 @@ function toggleModal(modal) {
 
 function openModal(modal) {
   modal.classList.add("modal__open");
+  document.addEventListener("click", closeFormOverlay);
+  document.addEventListener("keyup", closeFormEsc);
 };
 
 function closeModal(modal) {
   modal.classList.remove("modal__open");
+  document.removeEventListener("click", closeFormOverlay);
+  document.removeEventListener("keyup", isFormEsc);
 };
+
+function isFormEsc (e, action) {
+  const activeModal = document.querySelector(".modal__open");
+  if (e.key === 'Escape') {
+    action(activeModal);
+  }
+};
+
+function closeFormEsc (e) {
+  isFormEsc(e, closeModal);
+};
+
+function isOverlayClicked (e, action) {
+  const activeModal = document.querySelector(".modal__open");
+  if (e.target === activeModal) {
+    action(activeModal);
+  }; 
+};
+
+function closeFormOverlay (e) {
+  isOverlayClicked(e, closeModal)
+}
 
 addCardButton.addEventListener("click", () => {
   addForm.reset();
@@ -142,6 +168,7 @@ imageOpenCloseButton.addEventListener("click", () => {
 });
 
 function toggleButtonState (inputs, button, settings) {
+  console.log(1213);
   const {inactiveButtonClass} = settings;
   const isFormValid = inputs.every(input =>  {
    return input.validity.valid})
@@ -154,77 +181,3 @@ function toggleButtonState (inputs, button, settings) {
     button.disabled = 'disabled';
   }
 }
-
-function showError (input, settings) {
-  const {inputErrorClass, errorClass} = settings;
-  const error = input.validationMessage;
-  const errorElement = document.querySelector(`#${input.id}-error`);
-  console.log(errorElement, input, error);
-  errorElement.textContent = error;
-  errorElement.classList.add(inputErrorClass);
-  input.classList.add(errorClass);
-}
-
-function hideError (input, settings) {
-  const {inputErrorClass, errorClass} = settings;
-  const errorElement = document.querySelector(`#${input.id}-error`);
-  errorElement.textContent = "";
-  errorElement.classList.remove(inputErrorClass);
-  input.classList.remove(errorClass);
-}
-
-function checkValidity (input, settings) {
-  if (input.validity.valid) {
-    hideError (input, settings);
-  } else {
-    showError (input, settings);
-  }
-}
-
-function enableValidation (settings) {
-  const {formSelector, inputSelector, submitButtonSelector, ...rest} = settings;
-  const forms = [...document.querySelectorAll(formSelector)];
-  forms.forEach(form => {
-    form.addEventListener("submit", (e) => { 
-      e.preventDefault();
-    });
-    const inputs = [...form.querySelectorAll(inputSelector)];
-    const button = form.querySelector(submitButtonSelector);
-      inputs.forEach(input => {
-         input.addEventListener("input", () => {
-           checkValidity(input, rest);
-           toggleButtonState (inputs, button, rest)
-         })
-      });
-    console.log(inputs);
-  });
-}
-
-const config = {
-  formSelector: ".modal",
-  inputSelector: ".form__input",
-  submitButtonSelector: ".form__button",
-  inactiveButtonClass: "form__button_disabled",
-  inputErrorClass: "form__input_theme_error",
-  errorClass: "form__input-error"
-}
-
-enableValidation(config); 
-
-function closeImage(){
-  const modals = [...querySelectorAll(".modal")];
-    modals.forEach (modal => document.addEventListener("click", (e) =>{
-      if(modal.classList.contains("modal__open")) {
-        modal.classList.remove("modal__open");
-      }
-    }));
-  };
-  closeImage();
-
-    // function closeImage () {
-//   if (!target === popup) {
-//     closeModal(imageOpen)
-//   }
-// }
-
-// document.addEventListener("click", closeImage);
---
+++ validate.js
@@ -0,0 +1,57 @@
+function showError (input, settings) {
+    const {inputErrorClass, errorClass} = settings;
+    const error = input.validationMessage;
+    const errorElement = document.querySelector(`#${input.id}-error`);
+    console.log(errorElement, input, error);
+    errorElement.textContent = error;
+    errorElement.classList.add(inputErrorClass);
+    input.classList.add(errorClass);
+  }
+  
+  function hideError (input, settings) {
+    const {inputErrorClass, errorClass} = settings;
+    const errorElement = document.querySelector(`#${input.id}-error`);
+    errorElement.textContent = "";
+    errorElement.classList.remove(inputErrorClass);
+    input.classList.remove(errorClass);
+  }
+  
+  function checkValidity (input, settings) {
+    if (input.validity.valid) {
+      hideError (input, settings);
+    } else {
+      showError (input, settings);
+    }
+  }
+  
+  function enableValidation (settings) {
+    const {formSelector, inputSelector, submitButtonSelector, ...rest} = settings;
+    const forms = [...document.querySelectorAll(formSelector)];
+    forms.forEach(form => {
+      form.addEventListener("submit", (e) => { 
+        e.preventDefault();
+      });
+      const inputs = [...form.querySelectorAll(inputSelector)];
+      const button = form.querySelector(submitButtonSelector);
+        inputs.forEach(input => {
+           input.addEventListener("input", () => {
+             checkValidity(input, rest);
+             toggleButtonState (inputs, button, rest)
+           })
+        });
+      console.log(inputs);
+    });
+  }
+  
+  const config = {
+    formSelector: ".modal",
+    inputSelector: ".form__input",
+    submitButtonSelector: ".form__button",
+    inactiveButtonClass: "form__button_disabled",
+    inputErrorClass: "form__input_theme_error",
+    errorClass: "form__input-error"
+  }
+  
+  enableValidation(config); 
+  
+  
